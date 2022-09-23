7d677da957957d56ef9dabbbc259b7fc9e05b14d
acasasaez <acasasae@myuax.com>
2022-09-23 16:21:17 +0200

    Update Ejercicio_3.py

---
+++ Ejercicio_3.py
@@ -0,0 +1,8 @@
+def funcion():
+    Lista=[]
+    while True:
+        try:
+            a = int(input("Valor de inicio de lista"))
+            b =int(input("Valor de fin lista"))
+            c =int(input("Valor de diferencia entre valores consecutivos de lista"))
+            c!=0
