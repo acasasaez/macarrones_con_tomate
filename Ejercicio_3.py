a81c83d02107003c017f609b31547e0ae8898cc8
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-09-23 20:58:14 +0200

    Update Ejercicio_3.py

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -18,3 +18,11 @@ def funcion(Lista =[]):
             print("Error")
 
 funcion()
+funcion()
+#Para que se impriman las listas requeridas basta con introducir los parámetros a,b,c correctos
+#1ºNúmeros del 0 al 10: a = 0 b= 10 c=1
+#2ºNúmeros del -10 al 0: a = -10 b= 0 c=1
+#3ºNúmeros pares del 0 al 20  a = 0 b= 20 c=2
+#4ºNúmeros impares del -20 al 0 a = -19 b= 0 c=2
+#5ºMúltiplos de 5 del 0 al 50 a = 0 b= 50 c=5
+
