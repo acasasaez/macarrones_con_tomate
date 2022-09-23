cd69cc8da9c37ceb50b46aa8ac8da567e52a8ada
acasasaez <acasasae@myuax.com>
2022-09-23 16:21:43 +0200

    Update Ejercicio_3.py

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -6,3 +6,9 @@ def funcion():
             b =int(input("Valor de fin lista"))
             c =int(input("Valor de diferencia entre valores consecutivos de lista"))
             c!=0
+            for i in range (a,b+1,c):
+                Lista.append(i)
+                print (Lista)
+            break
+        except :
+            print("Error")
