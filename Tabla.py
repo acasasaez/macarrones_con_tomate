ead2163a92a0e297fe6cdd7206859de5c6f8b20b
Barroso03 <91721590+Barroso03@users.noreply.github.com>
2022-09-23 20:48:36 +0200

    Update Tabla.py

--- Tabla.py
+++ Tabla.py
@@ -1,16 +1,22 @@
 def funcion():
-    
     while True:
-        try:
-            a = int(input("valor"))
-            b =int(input("valor"))
+        while True:
+            try:
+                a = int(input("valor1"))
+                b =int(input("valor2"))
             
             
-            break
-        except :
-            print("Error")
-    if a >=1 and 9>=a and b >=1 and 9>=b:
-        for i in range (a):
-            for j in range (b):
-                print("*",end = "" )
+                break
+            except :
+                print("Error")
+           
+        if a >=1 and 9>=a and b >=1 and 9>=b:
+                for i in range (a):
+                    for j in range (b):
+                        print("*",end = "" )
+                break
+        
+        else:
+                print("Introduzca un número del 1 al 9")
 
+funcion()
